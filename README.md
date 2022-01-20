f3e884fe3acb499a89a9c90b24e703d4ac3b4e01
ericmackrodt <ericmackrodt@gmail.com>
2022-01-21 10:57:28 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-wgu8t4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wgu8t4)
---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+alert("come on!");
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
